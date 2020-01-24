148c110f1c8192e99a8bfb77e07c6928426a60d9
David Zeng <davzen@microsoft.com>
2020-01-24 12:34:29 +0800

    apply nexttick

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ var path = require('path')
 var app = express()
  
 app.use(express.static(path.join(__dirname, 'dist')))
-app.listen(8080);
+app.listen(8080, () => {
+    console.log("Browse http://localhost:8080/index.html")
+});
--- src/app.vue
+++ src/app.vue
@@ -49,9 +49,9 @@
             document.body.addEventListener("touchmove", function (evt) {
                 evt.preventDefault();
             }, false);
-            setTimeout(() => {
+            Vue.nextTick(() => {
                 this.bindTouch()
-            }, 200)
+            })
         }
     }
 </script>
